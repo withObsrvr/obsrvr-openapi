1aed832888bea613014703a9dc890238f140656c
Tillman Mosley III <tillman@withobsrvr.com>
2023-02-19 18:59:59 -0500

    Initial Stoplight commit

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NTUxMg/spectral.js?branch_slug=main&token=762e13ea-8342-408a-ac6f-f21b3b909dbb";
+export default { extends: ruleset };
